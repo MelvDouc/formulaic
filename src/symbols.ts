970389efef41a52e1abc4ea84cfe2b428ed8c886
MelvDouc <melv.douc@gmaiL.com>
2023-08-26 04:48:49 +0200

    Fix partial casting

--- src/symbols.ts
+++ src/symbols.ts
@@ -2,4 +2,5 @@ export const castFnSymbol = Symbol();
 export const nullableSymbol = Symbol();
 export const optionalSymbol = Symbol();
 export const partialSymbol = Symbol();
+export const cloneSymbol = Symbol();
 export const errorCheckersSymbol = Symbol();
